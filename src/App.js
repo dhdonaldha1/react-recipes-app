d1dfed11b260eaf4a7fff7fb66e37b44a2353458
dhdonaldha1 <donaldhas.email@gmail.com>
2024-11-07 23:36:51 -0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import React, { Component } from "react";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from "./pages/Home";
+import Recipes from './pages/Recipes';
+import SingleRecipe from './pages/SingleRecipe';
+import Default from './pages/Default';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
-function App() {
+class App extends Component {
+  render() {
   return (
-    <div>
-        Hello, welcome to the app. 
-    </div>
+    <Router>
+      <main>
+        <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/recipes" exact element={<Recipes />} />
+        <Route path="/recipes/:id" element={<SingleRecipe />} />
+        <Route component={<Default />} />
+      </Routes>
+      </main>
+    </Router>
   );
+}
 }
 
 export default App;
