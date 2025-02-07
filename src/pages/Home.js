7ff4c3a06c3d8a2c8f58fbba73d31e48271a6b05
dhdonaldha1 <donaldhas.email@gmail.com>
2025-02-07 11:04:15 -0600

    message

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 import Header from '../components/Header'
+import {Link} from 'react-router-dom'
 
 export default class Home extends Component
 {
     render() {
         return (
-            <Header />
+            <Header title="amazing recipes">
+                <Link to="recipes" className="text-uppercase btn btn-secondary btn0lg mt-3">
+                search recipes
+                </Link>
+                </Header>
         )
     }
 }
